355dd32ef51a9eab724187791cc1bea44fcc4441
paulo <pauloramos.04@hotmail.com>
2024-08-15 21:44:47 -0300

    feat: first commit

---
+++ src/test/java/saudeconectada/fatec/SaudeConectadaApplicationTests.java
@@ -0,0 +1,13 @@
+package saudeconectada.fatec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaudeConectadaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
